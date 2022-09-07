ed0dcfef4f077488d0f9198e68100aaf1b6fe764
Atulkumar9793 <atulkumar80551@gmail.com>
2022-09-07 14:11:21 +0530

    INdent

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -2,10 +2,10 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
-  return (
+    return (
     <View>
-      <Text className="text-red-500">HomeScreen</Text>
+    <Text className="text-red-500">HomeScreen</Text>
     </View>
-  )
+)
 }
 export default HomeScreen
